43674d725ec851a4f5e0fb56454301ce13f83024
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-03-02 11:55:33 -0500

    fixed fadeaway message after bug after success

--- js/resultsReturned.js
+++ js/resultsReturned.js
@@ -1,7 +1,7 @@
 (function($, Drupal) {
-    Drupal.ajax.prototype.commands.fadeAwayStatusMessage = function(ajax, response, status) {
-        window.setTimeout(function() {
-            $("#convio_signup_ajax_wrapper").children().fadeOut();
-          }, 2000);
-    };
+  Drupal.ajax.prototype.commands.fadeAwayStatusMessage = function(ajax, response, status) {
+    window.setTimeout(function() {
+      $("#" + response.targetElement).children().fadeOut();
+    }, 2000);
+  };
 }(jQuery, Drupal));
